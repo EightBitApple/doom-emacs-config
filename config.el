01cc84ce75521c8fb5382a15eef076f1779413be
The8BitApple <stefanharris16@gmail.com>
2024-06-09 16:26:23 +0100

    Made Org mode faces the same size.
    For a more uniform look and to fit more text into a buffer.

--- config.el
+++ config.el
@@ -48,16 +48,16 @@
 (setq org-ellipsis "⤵")
 
 (custom-set-faces
-  '(org-document-title ((t (:inherit outline-1 :height 1.5))))
-  '(org-document-info ((t (:inherit outline-1 :height 1.2))))
-  '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
-  '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
-  '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
-  '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
-  '(org-level-5 ((t (:inherit outline-5 :height 1.2))))
-  '(org-level-6 ((t (:inherit outline-5 :height 1.2))))
-  '(org-level-7 ((t (:inherit outline-5 :height 1.2))))
-  '(org-level-8 ((t (:inherit outline-5 :height 1.2)))))
+  '(org-document-title ((t (:inherit outline-1 :height 1.0))))
+  '(org-document-info ((t (:inherit outline-1 :height 1.0))))
+  '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+  '(org-level-6 ((t (:inherit outline-5 :height 1.0))))
+  '(org-level-7 ((t (:inherit outline-5 :height 1.0))))
+  '(org-level-8 ((t (:inherit outline-5 :height 1.0)))))
 
 (use-package! org-auto-tangle
   :defer t
