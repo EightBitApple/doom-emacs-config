0957520d43ee3d6d6febf0dad03246f2f9fe0b64
The8BitApple <stefanharris16@gmail.com>
2024-07-02 14:38:55 +0100

    Removed custom faces for Org mode.

--- config.el
+++ config.el
@@ -48,18 +48,6 @@
 (setq org-hide-emphasis-markers nil)
 (setq org-ellipsis "⤵")
 
-(custom-set-faces
-  '(org-document-title ((t (:inherit outline-1 :height 1.0))))
-  '(org-document-info ((t (:inherit outline-1 :height 1.0))))
-  '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
-  '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
-  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
-  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
-  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
-  '(org-level-6 ((t (:inherit outline-5 :height 1.0))))
-  '(org-level-7 ((t (:inherit outline-5 :height 1.0))))
-  '(org-level-8 ((t (:inherit outline-5 :height 1.0)))))
-
 (use-package! org-auto-tangle
   :defer t
   :hook (org-mode . org-auto-tangle-mode)
