d00c1b7c86e527d1afaed207c78f8c646e480329
EightBitApple <stefanharris16@gmail.com>
2025-08-14 18:56:29 +0100

    Revert "Moved from lsp-mode to lsp-bridge."
    This reverts commit a190edce0e3df917573bad8eb6b045ae1bd709a3.

--- packages.el
+++ packages.el
@@ -55,13 +55,3 @@
 (package! plantuml-mode :pin "348e83ff193051d5ad332642100dd704f6e2a6d2")
 (package! org-auto-tangle :pin "56e7afc35e4a6321d11c535600c287dbb1a90bc3")
 (package! gruber-darker-theme :pin "2e9f99c41fe8ef0557e9ea0f3b94ef50c68b5557")
-
-(when (package! lsp-bridge :pin "ff20c5c43b4e93801283079f73799dd5ed727ea4"
-        :recipe (:host github
-                 :repo "manateelazycat/lsp-bridge"
-                 :branch "master"
-                 :files ("*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
-                 ;; do not perform byte compilation or native compilation for lsp-bridge
-                 :build (:not compile)))
-(package! markdown-mode :pin "d51c469133d220823cc6ab50ff8e8743ed6e42fb")
-(package! yasnippet :pin "c1e6ff23e9af16b856c88dfaab9d3ad7b746ad37"))
